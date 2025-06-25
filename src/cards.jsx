50c9a914c102b13d1b78a8b5fc91a5341c012065
renee <156127959+reneexie01@users.noreply.github.com>
2025-06-26 07:17:47 +1000

    Created handleClick function to sendData back to parent and also to registerClick to begin logging pokemon clicked

--- src/cards.jsx
+++ src/cards.jsx
@@ -1,27 +1,49 @@
 import { useState, useEffect } from "react";
 export { Cards }
 
-function Cards({registerClick, count}) {
+function Cards({registerClick, count, onDataSend}) {
     const [image, setImage] = useState(null)
-    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    const [pokemonNumber, setPokemonNumber] = useState('')
 
     useEffect(() => {
-        fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}/`)
+        const randomNumber = Math.floor(Math.random() * 50) + 1;
+        setPokemonNumber(randomNumber);
+    }, [count])
+    
+    const sendData = () => {
+        onDataSend(pokemonNumber)
+    }
+
+    const handleClick = () => {
+        sendData();
+        registerClick();
+    }
+
+    useEffect(() => {
+        // Initial render of the setPokemonNumber useEffect will produce undefined so this needs to guard against it
+        if (!pokemonNumber) { 
+            return;
+        }
+
+        fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonNumber}/`)
         .then(function(response) {
-            console.log(response)
-            console.log('Random number', randomNumber)
             return response.json();
         })
         .then(function(response) {
-            console.log(response)
-            const sprite = response.sprites.other.dream_world.front_default;
+            const sprite = response.sprites.front_default;
+            if(!sprite) {
+                setTimeout(sprite, 1000)
+            }
             setImage(sprite);
         })
-    }, [count])
+        .catch((error) => {
+            console.error("Failed to catch Pokemon data:", error);
+        })
+    }, [pokemonNumber])
 
     return (
         <>
-         <div className="card" onClick={registerClick}>
+         <div className="card" onClick={handleClick}>
             <img src={image} alt="test" />
         </div>
         </>
--- src/components.jsx
+++ src/components.jsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Cards } from "./cards";
 export { Api }
 
 function Api() {
     const [count, setCount] = useState(0)
+    const [pokemonFound, setPokemonFound] = useState('')
 
     // Updates the count useState which is a dependency for the API useEffect
     const registerClick = () => {
@@ -11,16 +12,38 @@ function Api() {
         currentCount++
         setCount(currentCount)
     }
-    
+
+    //TODO: onClick must register the randomNumber that was generated and add it to an array
+    const handleChildData = (data) => {
+        setPokemonFound(data)
+    }
+
+    useEffect(() => {
+        console.log('pokemonFound', pokemonFound)
+    }, [pokemonFound])
+
+
     return (
         <>
         <Cards
             registerClick={registerClick}
             count={count}
+            onDataSend={handleChildData}
         />
          <Cards
             registerClick={registerClick}
             count={count}
+            onDataSend={handleChildData}
+        />
+         <Cards
+            registerClick={registerClick}
+            count={count}
+            onDataSend={handleChildData}
+        />
+         <Cards
+            registerClick={registerClick}
+            count={count}
+            onDataSend={handleChildData}
         />
         </>
     )
